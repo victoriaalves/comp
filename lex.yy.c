cc4054770cb005aab08a02e10970deccf1549f6b
Victoria Alves <victoriaelizabethaalves@gmail.com>
2019-08-25 10:52:14 -0300

    Add functions to scanner

--- lex.yy.c
+++ lex.yy.c
@@ -527,13 +527,14 @@ char *yytext;
 #include "tokens.h"
 
 int lineNumber = 1;
+int running;
 
 char buf[1000];
 char *c;
 
 
 
-#line 537 "lex.yy.c"
+#line 538 "lex.yy.c"
 
 #define INITIAL 0
 #define COMMENT 1
@@ -717,10 +718,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 16 "scanner.l"
+#line 17 "scanner.l"
 
 
-#line 724 "lex.yy.c"
+#line 725 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -805,213 +806,213 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 18 "scanner.l"
+#line 19 "scanner.l"
 return KW_BYTE;
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 19 "scanner.l"
+#line 20 "scanner.l"
 return KW_INT;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 20 "scanner.l"
+#line 21 "scanner.l"
 return KW_LONG;
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 21 "scanner.l"
+#line 22 "scanner.l"
 return KW_FLOAT;
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 22 "scanner.l"
+#line 23 "scanner.l"
 return KW_BOOL;
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 24 "scanner.l"
+#line 25 "scanner.l"
 return KW_IF;
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 25 "scanner.l"
+#line 26 "scanner.l"
 return KW_THEN;
 	YY_BREAK
 case 8:
 YY_RULE_SETUP
-#line 26 "scanner.l"
+#line 27 "scanner.l"
 return KW_ELSE;
 	YY_BREAK
 case 9:
 YY_RULE_SETUP
-#line 27 "scanner.l"
+#line 28 "scanner.l"
 return KW_WHILE;
 	YY_BREAK
 case 10:
 YY_RULE_SETUP
-#line 28 "scanner.l"
+#line 29 "scanner.l"
 return KW_FOR;
 	YY_BREAK
 case 11:
 YY_RULE_SETUP
-#line 29 "scanner.l"
+#line 30 "scanner.l"
 return KW_READ;
 	YY_BREAK
 case 12:
 YY_RULE_SETUP
-#line 30 "scanner.l"
+#line 31 "scanner.l"
 return KW_PRINT;
 	YY_BREAK
 case 13:
 YY_RULE_SETUP
-#line 31 "scanner.l"
+#line 32 "scanner.l"
 return KW_RETURN;
 	YY_BREAK
 case 14:
 YY_RULE_SETUP
-#line 32 "scanner.l"
+#line 33 "scanner.l"
 return KW_BREAK;
 	YY_BREAK
 case 15:
 YY_RULE_SETUP
-#line 34 "scanner.l"
+#line 35 "scanner.l"
 return OPERATOR_LE;
 	YY_BREAK
 case 16:
 YY_RULE_SETUP
-#line 35 "scanner.l"
+#line 36 "scanner.l"
 return OPERATOR_GE;
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
-#line 36 "scanner.l"
+#line 37 "scanner.l"
 return OPERATOR_EQ;
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
-#line 37 "scanner.l"
+#line 38 "scanner.l"
 return OPERATOR_DIF;
 	YY_BREAK
 case 19:
 YY_RULE_SETUP
-#line 39 "scanner.l"
+#line 40 "scanner.l"
 return LIT_TRUE;
 	YY_BREAK
 case 20:
 YY_RULE_SETUP
-#line 40 "scanner.l"
+#line 41 "scanner.l"
 return LIT_FALSE;
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
-#line 42 "scanner.l"
+#line 43 "scanner.l"
 {hashInsert(yytext); return TK_IDENTIFIER;}
 	YY_BREAK
 case 22:
 YY_RULE_SETUP
-#line 43 "scanner.l"
+#line 44 "scanner.l"
 {hashInsert(yytext); return LIT_INTEGER;}
 	YY_BREAK
 case 23:
 YY_RULE_SETUP
-#line 44 "scanner.l"
+#line 45 "scanner.l"
 {hashInsert(yytext); return LIT_FLOAT;}
 	YY_BREAK
 case 24:
 YY_RULE_SETUP
-#line 45 "scanner.l"
+#line 46 "scanner.l"
 {hashInsert(yytext); return LIT_CHAR;}
 	YY_BREAK
 case 25:
 YY_RULE_SETUP
-#line 46 "scanner.l"
+#line 47 "scanner.l"
 {BEGIN(STRING); c = buf; *c++ = *yytext; return LIT_STRING;}
 	YY_BREAK
 case 26:
 YY_RULE_SETUP
-#line 48 "scanner.l"
+#line 49 "scanner.l"
 return yytext[0];
 	YY_BREAK
 case 27:
 YY_RULE_SETUP
-#line 50 "scanner.l"
+#line 51 "scanner.l"
 
 	YY_BREAK
 case 28:
 /* rule 28 can match eol */
 YY_RULE_SETUP
-#line 51 "scanner.l"
+#line 52 "scanner.l"
 ++lineNumber;
 	YY_BREAK
 case 29:
 YY_RULE_SETUP
-#line 52 "scanner.l"
+#line 53 "scanner.l"
 
 	YY_BREAK
 case 30:
 YY_RULE_SETUP
-#line 53 "scanner.l"
+#line 54 "scanner.l"
 
 	YY_BREAK
 case 31:
 YY_RULE_SETUP
-#line 54 "scanner.l"
+#line 56 "scanner.l"
 BEGIN(COMMENT);
 	YY_BREAK
 case 32:
 YY_RULE_SETUP
-#line 56 "scanner.l"
+#line 58 "scanner.l"
 BEGIN(INITIAL);
 	YY_BREAK
 case 33:
 /* rule 33 can match eol */
 YY_RULE_SETUP
-#line 57 "scanner.l"
+#line 59 "scanner.l"
 ++lineNumber;
 	YY_BREAK
 case 34:
 YY_RULE_SETUP
-#line 58 "scanner.l"
+#line 60 "scanner.l"
 
 	YY_BREAK
 case 35:
 YY_RULE_SETUP
-#line 60 "scanner.l"
+#line 62 "scanner.l"
 {*c++ = *yytext; *c = 0; hashInsert(buf); BEGIN(INITIAL);}
 	YY_BREAK
 case 36:
 YY_RULE_SETUP
-#line 61 "scanner.l"
+#line 63 "scanner.l"
 {*c++ = '\n';}
 	YY_BREAK
 case 37:
 YY_RULE_SETUP
-#line 62 "scanner.l"
+#line 64 "scanner.l"
 {*c++ = '\t'; }
 	YY_BREAK
 case 38:
 YY_RULE_SETUP
-#line 63 "scanner.l"
+#line 65 "scanner.l"
 {*c++ = '\"';}
 	YY_BREAK
 case 39:
 YY_RULE_SETUP
-#line 64 "scanner.l"
+#line 66 "scanner.l"
 {*c++ = *yytext;}
 	YY_BREAK
 case 40:
 /* rule 40 can match eol */
 YY_RULE_SETUP
-#line 65 "scanner.l"
+#line 67 "scanner.l"
 {++lineNumber; *c++ = '\n';}
 	YY_BREAK
 case 41:
 YY_RULE_SETUP
-#line 67 "scanner.l"
+#line 69 "scanner.l"
 ECHO;
 	YY_BREAK
-#line 1015 "lex.yy.c"
+#line 1016 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(COMMENT):
 case YY_STATE_EOF(STRING):
@@ -2010,11 +2011,19 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 67 "scanner.l"
+#line 69 "scanner.l"
 
 
 
 int running = 1;
+
+int getLineNumber(void){
+    return lineNumber;
+}
+
+int isRunning(void){
+    return running;
+}
 
 int yywrap()
 {
@@ -2023,8 +2032,12 @@ int yywrap()
 }
 
 void initMe(void){
+	lineNumber = 1;
+	running = 1;
 	hashInit();
 }
 
 #include "main.c"
+
+
 
--- main.c
+++ main.c
@@ -12,64 +12,64 @@ int main()
 		switch(tok)
 		{
 			case KW_BYTE:
-				fprintf(stderr, "Found keyword 'byte'.\n"); break;
+				fprintf(stderr, "Line %d: BYTE\n", getLineNumber()); break;
 			case KW_INT:
-				fprintf(stderr, "Found keyword 'int'.\n"); break;
+				fprintf(stderr, "Line %d: INT\n", getLineNumber()); break;
 			case KW_LONG:
-				fprintf(stderr, "Found keyword 'long'.\n"); break;
+				fprintf(stderr, "Line %d: LONG\n", getLineNumber()); break;
 			case KW_FLOAT:
-				fprintf(stderr, "Found keyword 'float'.\n"); break;
+				fprintf(stderr, "Line %d: FLOAT\n", getLineNumber()); break;
 			case KW_BOOL:
-				fprintf(stderr, "Found keyword 'bool'.\n"); break;
+				fprintf(stderr, "Line %d: BOOL\n", getLineNumber()); break;
 			case KW_IF:
-				fprintf(stderr, "Found keyword 'byte'.\n"); break;
+				fprintf(stderr, "Line %d: IF\n", getLineNumber()); break;
 			case KW_THEN:
-				fprintf(stderr, "Found keyword 'then'.\n"); break;
+				fprintf(stderr, "Line %d: THEN\n", getLineNumber()); break;
 			case KW_ELSE:
-				fprintf(stderr, "Found keyword 'else'.\n"); break;
+				fprintf(stderr, "Line %d: ELSE\n", getLineNumber()); break;
 			case KW_WHILE:
-				fprintf(stderr, "Found keyword 'while'.\n"); break;
+				fprintf(stderr, "Line %d: WHILE\n", getLineNumber()); break;
 			case KW_FOR:
-				fprintf(stderr, "Found keyword 'for'.\n"); break;
+				fprintf(stderr, "Line %d: FOR\n", getLineNumber()); break;
 			case KW_READ:
-				fprintf(stderr, "Found keyword 'read'.\n"); break;
+				fprintf(stderr, "Line %d: READ\n", getLineNumber()); break;
 			case KW_PRINT:
-				fprintf(stderr, "Found keyword 'print'.\n"); break;
+				fprintf(stderr, "Line %d: PRINT\n", getLineNumber()); break;
 			case KW_RETURN:
-				fprintf(stderr, "Found keyword 'return'.\n"); break;
+				fprintf(stderr, "Line %d: RETURN\n", getLineNumber()); break;
 			case KW_BREAK:
-				fprintf(stderr, "Found keyword 'break'.\n"); break;
+				fprintf(stderr, "Line %d: BREAK\n", getLineNumber()); break;
 
 			case OPERATOR_LE:
-				fprintf(stderr, "<=.\n"); break;
+				fprintf(stderr, "Line %d: <=\n", getLineNumber()); break;
 			case OPERATOR_GE:
-				fprintf(stderr, ">=.\n"); break;
+				fprintf(stderr, "Line %d: >=\n", getLineNumber()); break;
 			case OPERATOR_EQ:
-				fprintf(stderr, "==.\n"); break;
+				fprintf(stderr, "Line %d: ==\n", getLineNumber()); break;
 			case OPERATOR_DIF:
-				fprintf(stderr, "!=.\n"); break;
+				fprintf(stderr, "Line %d: !=\n", getLineNumber()); break;
 
 			case LIT_TRUE:
-				fprintf(stderr, "Literal TRUE.\n"); break;
+				fprintf(stderr, "Line %d: TRUE\n", getLineNumber()); break;
 			case LIT_FALSE:
-				fprintf(stderr, "Literal FALSE.\n"); break;
+				fprintf(stderr, "Line %d: FALSE\n", getLineNumber()); break;
 			case TK_IDENTIFIER:
-				fprintf(stderr, "Identifier.\n"); break;
+				fprintf(stderr, "Line %d: IDENTIFIER\n", getLineNumber()); break;
 			case LIT_INTEGER:
-				fprintf(stderr, "Literal Integer.\n"); break;
+				fprintf(stderr, "Line %d: LITERAL INTEGER\n", getLineNumber()); break;
 			case LIT_FLOAT:
-				fprintf(stderr, "Literal Float.\n"); break;
+				fprintf(stderr, "Line %d: LITERAL FLOAT\n", getLineNumber()); break;
 
 			case LIT_CHAR:
-				fprintf(stderr, "Literal Char.\n"); break;
+				fprintf(stderr, "Line %d: LITERAL\n", getLineNumber()); break;
 			case LIT_STRING:
-				fprintf(stderr, "Literal String.\n"); break;
+				fprintf(stderr, "Line %d: LITERAL STRING\n", getLineNumber()); break;
 			//case TOKEN_ERROR:
 			//	fprintf(stderr, "Error.\n"); break;
 
 
 			default:
-				fprintf(stderr, "Token ascii %c\n", tok); break;
+				fprintf(stderr, "Line %d: TOKEN ASCII\n", getLineNumber()); break;
 		}
 
 	}
