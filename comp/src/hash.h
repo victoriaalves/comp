42765f28d2826c089214d1a481330e0cc6468258
Victoria Alves <victoriaelizabethaalves@gmail.com>
2019-10-08 21:29:32 -0300

    Starting etapa 4 with data types

--- comp/src/hash.h
+++ comp/src/hash.h
@@ -4,6 +4,22 @@
 
 #define HASH_SIZE 997
 
+#define SYMBOL_IDENTIFIER 1
+#define SYMBOL_LIT_INT 2
+#define SYMBOL_LIT_FLOAT 3
+#define SYMBOL_LIT_CHAR 4
+#define SYMBOL_LIT_STRING 5
+
+#define SYMBOL_VAR 6
+#define SYMBOL_VEC 7
+#define SYMBOL_FUNC 8
+#define SYMBOL_PARAM 9
+
+#define DATATYPE_BYTE 10
+#define DATATYPE_INT 11
+#define DATATYPE_FLOAT 12
+#define DATATYPE_BOOL 13
+
 typedef struct hash_node
 {
 	int type;
@@ -15,8 +31,9 @@ void hashInit(void);
 
 int hasAdress(char *text);
 HASH_NODE *hashFind(char *text);
-HASH_NODE *hashInsert(char *text);
+HASH_NODE *hashInsert(char *text, int type);
 void hashPrint(void);
 
 #endif
 //END
+
